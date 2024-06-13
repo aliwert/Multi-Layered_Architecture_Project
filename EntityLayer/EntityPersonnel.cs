af2e29f482634c3b58344ded0e80bcc5719e0144
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-13 19:14:27 +0300

    Add project files.

---
+++ EntityLayer/EntityPersonnel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityPersonnel
+    {
+        private int id;
+        private string name;
+        private string surname;
+        private string city;
+        private string duty;
+        private short maas;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string City { get => city; set => city = value; }
+        public string Duty { get => duty; set => duty = value; }
+        public short Maas { get => maas; set => maas = value; }
+    }
+}
