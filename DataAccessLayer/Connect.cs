670c512119f68c640ce175c0a24a411ce8c09db0
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-16 23:08:38 +0300

    listing with data access layer

---
+++ DataAccessLayer/Connect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class Connect
+    {
+
+        public static SqlConnection cn = new SqlConnection(@"Data Source=DESKTOP-LRMEISB\\SQLEXPRESS;Initial Catalog=DbPersonnel;Integrated Security=True");
+        
+    }
+}
---
+++ DataAccessLayer/DALPersonnel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+namespace DataAccessLayer
+{
+    public class DALPersonnel
+    {
+        public static List <EntityPersonnel> PersonnelList()
+        {
+            List<EntityPersonnel> deger = new List<EntityPersonnel>();
+            SqlCommand cmd = new SqlCommand("Select * From TBL_INFORMATION", Connect.cn);
+            if (cmd .Connection.State != ConnectionState.Open)
+            {
+                cmd.Connection.Open();
+            }
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                EntityPersonnel ent = new EntityPersonnel();
+                ent.Id = int.Parse(dr["ID"].ToString());
+                ent.Name = dr["NAME"].ToString();
+                ent.Surname = dr["SURNAME"].ToString();
+                ent.Duty = dr["DUTY"].ToString();
+                ent.Maas = short.Parse(dr["SALARY"].ToString());
+                deger.Add(ent);
+                
+            }
+            dr.Close();
+            return deger;
+        }
+    }
+}
