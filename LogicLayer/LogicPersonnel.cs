5f61f2a6ade45ee0134b758fedea761a21f5a7d4
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-17 19:30:29 +0300

    logic layer

---
+++ LogicLayer/LogicPersonnel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataAccessLayer;
+
+
+namespace LogicLayer
+{
+    public class LogicPersonnel
+    {
+        public static List<EntityPersonnel> LLPersonnelList()
+        {
+
+        return DALPersonnel.PersonnelList(); 
+        
+        }
+
+    }
+}
